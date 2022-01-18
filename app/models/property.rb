4e189c17f2b2c72bc70f1196fd346a98c27eeb79
shunpikun <moirir0906@gmail.com>
2022-01-18 15:10:43 +0900

    バリデーションの設定

--- app/models/property.rb
+++ app/models/property.rb
@@ -5,5 +5,12 @@ class Property < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :frequency
 
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :info
+    validates :storage
+  end
+
   validates :frequency_id, numericality: { message: "can't be blank" }
 end
