037dde538dc2e1bbb599051c2b0aedddf077c2d3
shunpikun <moirir0906@gmail.com>
2022-01-20 14:27:25 +0900

    登録機能の実装

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,7 +1,27 @@
 class PropertiesController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+    @properties = Property.all.order('created_at DESC')
   end
 
   def new
+    @property = Property.new
+  end
+
+  def create
+    @property = Property.new(property_params)
+    if @property.valid?
+      @property.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def property_params
+    params.require(:property).permit(:name, :info, :frequency_id, :storage, :image).merge(user_id: current_user.id)
   end
 end
