1cf656cd4f23a7fb02445c68b2a3cdd0fd74ae94
shunpikun <moirir0906@gmail.com>
2022-01-29 15:58:46 +0900

    updateアクションとルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "properties#index"
-  resources :properties, only: [:index, :new, :create, :show, :edit]
+  resources :properties, only: [:index, :new, :create, :show, :edit, :update]
   resources :users, only: [:edit, :update]
 end
