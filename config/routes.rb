2238ddf79b78227cd61b20604112307e4493bd88
shunpikun <moirir0906@gmail.com>
2022-01-29 14:39:17 +0900

    編集画面の作成とルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "properties#index"
-  resources :properties, only: [:index, :new, :create, :show]
+  resources :properties, only: [:index, :new, :create, :show, :edit]
   resources :users, only: [:edit, :update]
 end
