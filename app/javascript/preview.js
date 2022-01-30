d8746a1666068c7ad40fca2bd420087deb89eb13
shunpikun <moirir0906@gmail.com>
2022-01-30 16:41:05 +0900

    画像のHTMLの作成

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -2,14 +2,15 @@ document.addEventListener('DOMContentLoaded', function(){
   const propertyForm = document.getElementById('new_property');
   const previewList = document.getElementById('previews');
   if (!propertyForm) return null;
-  console.log("preview.jsが読み込まれました");
 
   const fileField = document.querySelector('input[type="file"][name="property[image]"]');
   fileField.addEventListener('change', function(e){
-    console.log("input要素で値の変化が起きました");
     console.log(e.target.files[0]);
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
-    console.log(blob);
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
   });
 });
