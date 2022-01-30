7370e3fcd0ac5c1e444ab9603fcd0023f551418c
shunpikun <moirir0906@gmail.com>
2022-01-30 16:34:12 +0900

    選択した画像情報の取得

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -3,4 +3,13 @@ document.addEventListener('DOMContentLoaded', function(){
   const previewList = document.getElementById('previews');
   if (!propertyForm) return null;
   console.log("preview.jsが読み込まれました");
+
+  const fileField = document.querySelector('input[type="file"][name="property[image]"]');
+  fileField.addEventListener('change', function(e){
+    console.log("input要素で値の変化が起きました");
+    console.log(e.target.files[0]);
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+    console.log(blob);
+  });
 });
