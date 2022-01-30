8e59a0c9827622b32fd357a3016eb4ddf3227397
shunpikun <moirir0906@gmail.com>
2022-01-30 16:21:22 +0900

    新規登録と編集画面のみpreview.jsが読み込まれる設定

---
+++ app/javascript/preview.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const propertyForm = document.getElementById('new_property');
+  const previewList = document.getElementById('previews');
+  if (!propertyForm) return null;
+  console.log("preview.jsが読み込まれました");
+});
