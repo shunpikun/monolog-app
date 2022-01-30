8b0baff7a2f4631338b912874fca0183323e9809
shunpikun <moirir0906@gmail.com>
2022-01-30 16:48:26 +0900

    プレビュー機能の実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -5,6 +5,10 @@ document.addEventListener('DOMContentLoaded', function(){
 
   const fileField = document.querySelector('input[type="file"][name="property[image]"]');
   fileField.addEventListener('change', function(e){
+    const alreadyPreview = document.querySelector('.preview');
+    if (alreadyPreview) {
+      alreadyPreview.remove();
+    };
     console.log(e.target.files[0]);
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
@@ -12,5 +16,9 @@ document.addEventListener('DOMContentLoaded', function(){
     previewWrapper.setAttribute('class', 'preview');
     const previewImage = document.createElement('img');
     previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
   });
 });
