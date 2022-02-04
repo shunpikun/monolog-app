2d955e3fc3749b36cb558a1374983a786d3e781f
shunpikun <moirir0906@gmail.com>
2022-02-04 14:34:03 +0900

    gemの導入とルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "properties#index"
   resources :properties
+    collection do
+      get 'seach'
+    end
   resources :users, only: [:edit, :update]
 end
